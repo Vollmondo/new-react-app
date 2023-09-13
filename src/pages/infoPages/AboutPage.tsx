65fd94c2e0eb800636aa949b84029b9a0927be1c
Vollmondo <dmitrifom74@gmail.com>
2023-09-13 10:25:26 +0500

    updated about page

--- src/pages/infoPages/AboutPage.tsx
+++ src/pages/infoPages/AboutPage.tsx
@@ -1,11 +1,62 @@
 import React from "react";
 import { BasePage } from "../basePage/BasePage";
+import CompanyRequisites from "../../components/contacts/Requisites/Company_Requisites";
+import './AboutPage.css'
 
 export function AboutPage(){
     return(
         <>
             <BasePage>
-                <div className="about">Lorem ipsum, dolor sit amezzt consectetur adipisicing elit. Adipisci vero velit quisquam quia enim, ratione odio exercitationem sint expedita delectus est voluptate, accusamus earum maxime ipsum minima temporibus in vitae!</div>
+                <div className="about">
+                    <h1>О НАС</h1>
+                    <p className="about-p"></p>
+                    <p className="about-p">Lorem ipsum, dolor sit amezzt consectetur adipisicing elit. Adipisci vero velit quisquam quia enim, ratione odio exercitationem sint expedita delectus est voluptate, accusamus earum maxime ipsum minima temporibus in vitae!</p>
+                </div>
+                <h2 className="about-reqTableHeader">Реквизиты организации:</h2>
+                <table className="about-reqTable">
+                    <tbody>
+                        <tr>
+                            <td>Названеие организации:</td>
+                            <td>ООО {CompanyRequisites.title}</td>
+                        </tr><tr>
+                            <td>Адрес регистрации:</td>
+                            <td>{CompanyRequisites.reg_adress}</td>
+                        </tr><tr>
+                            <td>Адрес для отправки почтовой корреспонденции:</td>
+                            <td>{CompanyRequisites.post_adress}</td>
+                        </tr><tr>
+                            <td>ОГРН:</td>
+                            <td>{CompanyRequisites.OGRN}</td>
+                        </tr><tr>
+                            <td>ИНН/КПП:</td>
+                            <td>{CompanyRequisites.INN} / {CompanyRequisites.KPP}</td>
+                        </tr><tr>
+                            <td>ОКПО:</td>
+                            <td>{CompanyRequisites.OKPO}</td>
+                        </tr><tr>
+                            <td>ОКВЭД:</td>
+                            <td>{CompanyRequisites.OKVED}</td>
+                        </tr><tr>
+                            <td>Р/сч:</td>
+                            <td>{CompanyRequisites.rasS4et}</td>
+                        </tr><tr>
+                            <td>Кор/сч:</td>
+                            <td>{CompanyRequisites.corS4et}</td>
+                        </tr><tr>
+                            <td>БИК Банка:</td>
+                            <td>{CompanyRequisites.BIK}</td>
+                        </tr><tr>
+                            <td>Наименование Банка:</td>
+                            <td>{CompanyRequisites.bank}</td>
+                        </tr><tr>
+                            <td>ФИО/должность руководителя организации:</td>
+                            <td>{CompanyRequisites.genDirName}</td>
+                        </tr><tr>
+                            <td>ФИО главного бухгалтера организации:</td>
+                            <td>{CompanyRequisites.glavBuhName}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </BasePage>
         </>
     )
