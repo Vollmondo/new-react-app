193ee8098647249f21113b12b0eceab8117b4e19
Vollmondo <dmitrifom74@gmail.com>
2023-09-20 14:18:25 +0500

    added update cart function

--- src/pages/userPages/CartPage.tsx
+++ src/pages/userPages/CartPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BasePage } from "../basePage/BasePage";
 import "./CartPage.css";
 import { useAppSelector, useAppDispatch } from "../../store/hooks";
-import { getTotalPrice, removeFromCart } from "../../store/Cart.Slice";
+import { getTotalPrice, removeFromCart, updateQuantity } from "../../store/Cart.Slice";
 
 export function CartPage() {
   const dispatch = useAppDispatch();
@@ -10,7 +10,11 @@ export function CartPage() {
   const products = useAppSelector((state) => state.products.products);
   const items = useAppSelector((state) => state.cart.items);
   const totalPrice = useAppSelector(getTotalPrice);
-
+  
+  function onQuantityChanged(e: React.FocusEvent<HTMLInputElement>, id: string){
+    const quantity = Number(e.target.value) || 0;
+    dispatch(updateQuantity({id, quantity}))
+  }
     return(
         <BasePage>
             <div>
@@ -29,7 +33,7 @@ export function CartPage() {
           <tr>
             <td>{products[id].title}</td>
             <td>
-              <input type="text" className='input' defaultValue={quantity} />
+              <input type="number" className='input' defaultValue={quantity} onBlur={(e) => {onQuantityChanged(e, id)}}/>
             </td>
             <td>${products[id].price}</td>
             <td>
@@ -52,9 +56,7 @@ export function CartPage() {
         <button className='button' type="submit">
           Checkout
         </button>
-      </form>
-                <p>Количество выбранных товаров: </p>
-                
+      </form>                
             </div>
         </BasePage>
     )
