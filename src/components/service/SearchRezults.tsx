0b11b2b43ef920f081c66eb4efafcdcc5d763ac0
Vollmondo <dmitrifom74@gmail.com>
2023-09-26 16:53:05 +0500

    added another variant of product representation

--- src/components/service/SearchRezults.tsx
+++ src/components/service/SearchRezults.tsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BasePage } from "../../pages/basePage/BasePage";
-import { Product } from "../../pages/catalogPages/products/Product";
+import { ProductRow } from "../../pages/catalogPages/products/ProductRow";
 import { useAppSelector } from "../../store/hooks";
 import { IProduct } from "../../models";
+import { Search } from "./Search";
 
 export function SearchResults() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get("query");
   const searchResults = useAppSelector((state) => location.state?.results || []);
-
+  
   return (
     <BasePage>
       <div className="searchResults-container">
         <h2>Результаты поиска для запроса: {query}</h2>
-        {searchResults.map((product: IProduct) => (
-          <Product key={product._id} product={product} />
-        ))}
+        {searchResults.length > 0 ? (
+          searchResults.map((product: IProduct) => (
+            <ProductRow key={product._id} product={product} />
+          ))
+        ) : (
+          <p>По вашему запросу не найдено ни одного товара</p>
+        )}
       </div>
     </BasePage>
   );
---
+++ src/pages/catalogPages/products/ProductRow.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { IProduct } from "../../../models";
+import './ProductRow.css';
+import { Link } from "react-router-dom";
+import { FavButton } from "../../../components/service/FavButton";
+
+interface ProductProps {
+  product: IProduct
+}
+
+export function ProductRow({ product }: ProductProps) {
+  const storedUserJSON = localStorage.getItem("userJSON");
+  const [userId, setUserId] = useState<string>("");
+
+  useEffect(() => {
+    if (storedUserJSON) {
+      const user = JSON.parse(storedUserJSON);
+      setUserId(user._id);
+    }
+  }, [storedUserJSON]);
+
+  return (
+    <div className="productItem-row" key={product._id}>
+      <div className="productItem-row-img-container">
+        <img className="productItem-row-img" src={product.image} alt="Фото товара" loading="lazy" />
+      </div>
+      <div className="productItem-row-text-container">
+        <Link to={`/cat/${product._id}`} key={product._id}>
+            <p className="productItem-row-text row-title">{product.title}</p>
+        </Link>
+      </div>
+      <div className="productItem-row-text-container">
+        <p className="productItem-row-text row-price">&#127820;&nbsp;{product.price}</p>
+      </div>
+      <div className="productItem-row-icon-container">
+        {userId && <FavButton favProduct={product} userId={userId} />}
+      </div>
+      <div className="productItem-row-icon-container">
+        <button className=""></button>
+      </div>
+    </div>
+  );
+}
