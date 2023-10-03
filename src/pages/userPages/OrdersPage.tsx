a4edce3929b28bf3ff05d56eee243f5e9559d77f
Vollmondo <dmitrifom74@gmail.com>
2023-10-03 14:58:03 +0500

    updated order date\time visualization and order table styles

--- src/pages/userPages/OrdersPage.tsx
+++ src/pages/userPages/OrdersPage.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { BasePage } from "../basePage/BasePage";
-import { getCustomersOrders } from "../../api/api";
-import { ICartItem, IOrder } from "../../models";
+import { getCustomersOrders, getProductItem } from "../../api/api";
+import { ICartItem, IOrder, IProduct } from "../../models";
 import { useAppSelector } from "../../store/hooks";
+import "./OrdersPage.css";
 
 export function OrdersPage() {
     const [orders, setOrders] = useState<IOrder[]>([]);
     const user = useAppSelector((state) => state.user?.user);
+    const [productItems, setProductItems] = useState<IProduct[]>([]);
+
 
     useEffect(() => {
         if (user) {
@@ -22,38 +25,74 @@ export function OrdersPage() {
             }
         }
     }, [user]);
+    
+    useEffect(() => {
+        const fetchProductItems = async () => {
+          const items = await Promise.all(
+            orders.flatMap((order) =>
+              order.items.map((item) => getProductItem(item.id))
+            )
+          );
+          setProductItems(items);
+        };
+    
+        fetchProductItems();
+      }, [orders]);
           
-    return (
+      const formatDateTime = (dateTimeString: string | number | Date) => {
+        const dateTime = new Date(dateTimeString);
+        const formattedDate = dateTime.toLocaleDateString("ru-RU");
+        const formattedTime = dateTime.toLocaleTimeString("ru-RU", {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        return `${formattedDate} ${formattedTime}`;
+      };
+
+      return (
         <BasePage>
-            <h1>Заказы</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Номер заказа</th>
-                        <th>Товары</th>
-                        <th>Дата заказа</th>
-                        <th>Статус</th>
-                        <th>Общая стоимость</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders.map((order) => (
-                        <tr key={order._id}>
-                            <td>{order._id}</td>
-                            <td>
-                                <ul>
-                                {Array.isArray(order.items) && order.items.map((item: ICartItem) => (
-                                    <li key={item.id}>{item.id}</li>
-                                ))}
-                                </ul>
-                            </td>
-                            <td>{order.timestamp}</td>
-                            <td>{order.status}</td>
-                            <td>{order.totalprice}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+          <h1>Ваши заказы</h1>
+          <table className="orders-table">
+            <thead>
+              <tr>
+                <th className="colID">ID заказа</th>
+                <th className="colGoods">Состав заказа</th>
+                <th className="colDate">Дата заказа</th>
+                <th className="colStatus">Статус</th>
+                <th className="colSum">Общая стоимость</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order._id} className="orders-table-row">
+                    <td>{order._id}</td>
+                    <td>
+                        <table className="orders-table-items">
+                            <thead>
+                                <tr>
+                                    <th>Наименование</th>
+                                    <th>Кол-во</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {Array.isArray(order.items) &&
+                                    order.items.map((item: ICartItem, index: number) => (
+                                        <tr key={item.id}>
+                                            <td>{productItems[index]?.title}</td>
+                                            <td>{item.quantity}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </td>
+                    <td>{formatDateTime(order.timestamp)}</td>
+                    <td>{order.status}</td>
+                    <td>{order.totalprice}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </BasePage>
-    );
-}
+      );
+    }
