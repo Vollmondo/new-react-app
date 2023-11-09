05a856d9285f53f212b5324631e3f92ae14093a4
Vollmondo <dmitrifom74@gmail.com>
2023-11-09 09:53:30 +0500

    configuring jest / coverage check

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,29 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/*',
+    'src/**/*.ts?(x)',
+    '!src/index.tsx',
+    '!src/App.tsx',
+],
+  coverageDirectory: '<rootDir>/reports/coverage',
+  coverageProvider: 'v8',
+  coverageReporters: [
+    [
+        'html',
+        {
+            subdir: 'html',
+        },
+    ],
+  ],
+  globals: {
+    __webpack_public_path__: '__webpack_public_path__',
+  },
 };
