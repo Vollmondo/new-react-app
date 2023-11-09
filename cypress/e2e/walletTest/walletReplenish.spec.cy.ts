0813b6464b01e6d29ae4791aa01e620781f9fe74
Vollmondo <dmitrifom74@gmail.com>
2023-11-09 08:27:54 +0500

    Update walletReplenish.spec.cy.ts

--- cypress/e2e/walletTest/walletReplenish.spec.cy.ts
+++ cypress/e2e/walletTest/walletReplenish.spec.cy.ts
@@ -5,7 +5,7 @@ describe('работоспособность функции пополнения
     cy.get('input[name="username"]').first().type('123123', { force: true });
     cy.get('input[name="password"]').first().type('111111', { force: true });
     cy.get('button[id="loginbutton"]').first().click({ force: true });
-    cy.wait(2000);
+    cy.wait(1000);
     cy.url().should('not.include', 'login');
     // переходим на домашнюю страницу
     cy.visit('http://localhost:3000');
@@ -13,22 +13,27 @@ describe('работоспособность функции пополнения
   });
 
   it('проверяем работоспособность функции пополнения кошелька', () => {
+    cy.get('#walletValue').invoke('text').then((oldValue) => {
+      cy.wait(1000)
+      console.log('oldValue:', oldValue)
     
-    const oldValue = cy.get('#walletValue').invoke('val').then()
-      cy.wait(2000)
-      console.log(oldValue)
       cy.get('.header-personal-block')
         .find('img[alt="wallet"]')
+        .first()
         .click({ force: true });
+      cy.wait(1000)
 
       cy.get('input[name="debet"]').first().type('5', { force: true });
       cy.get('.addCoins-btn').first().click({ force: true });
-
-      cy.get('#walletValue').invoke('val').should((newValue) => {
-        console.log(newValue);})
-        cy.wait(2000)
-
+      cy.get('.modal-window').click({ force: true });
+    
+      cy.get('#walletValue').invoke('text').then((newValue) => {
+        cy.wait(1000)
+        console.log('newValue:', newValue)
+    
         expect(Number(oldValue) + 5).to.equal(Number(newValue));
       });
     });
+  });
+})
   
